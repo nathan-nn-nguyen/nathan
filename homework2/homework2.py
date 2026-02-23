e6a357e539d7f250cb4363107dea6b1e7e7fffe9
Nathan Nguyen <nathan_nn_nguyen@berkeley.edu>
2026-02-22 19:09:29 -0800

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,73 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+    #Git is a way to store data and can be called in commands to save data. Github is a remote repository for code, and gitbash is a terminal in which to call commands.
+
+# 2) What’s the difference between the terminal and the command line?
+    # They're both ways to give commands to the computer, but the terminal is text only, and lets you run a shell and debug. On the other hand the command line is where the actual prompts are written out to be sent. 
+
+# 3) How does Windows PowerShell differ from Git Bash?
+    #They're both terminals used to send out commands. However Git Bash is more similar to typical unix environments, like the default systems on Mac computers. 
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+    #Python is a type of coding language and file type, while anaconda is a distribution that holds different tool packages for coding. Conda is a specific package manage contained within Anaconda
+
+# 5) What is VS Code? 
+    #VS Code is a code editor that allows you to write and execute code within.
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+    #It is an open source application that is used to make and share documents with live code in forms such as data analysis and data visualization. Juypter lab is a similar updated version, but is different because it is a little more advanced and can hold multiple tabs in a window
+
+# 7) What does ~/ mean?
+    #It represents the home directory
+
+# 8) What’s the difference between an absolute path and a relative path?
+    #An absolute path leads straight to a different file or directory, while a relative path has to go through differnt family branches in order to get to the file or directory intended.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+    #Relative: cd cpurse_assignments
+               #cd homework2
+                
+    #Direct: cd course_assignments/homework2
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+    #cd ..
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+    #It would delete the current directory.
+
+# 12) What do the following commands do?
+# git add
+    #It adds new files in the directory into the Git staging area to be accepted to be commited.
+# git commit
+    #It saves the files that were added and saves the progress.
+# git push
+    #It adds the file to a remoter repository to be used by others.
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+    #git add . adds all the files in the current dirrectory to the Git Staging Area, while git add <file> adds only the single file into the staging area.  
+
+# 14) What do "git status" and "git log -1" do?
+    #git status outputs a list of files that have been yet to be added and commited, git log -1 displays the most recent thing to have been commited
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+    #Cloning creates a new repository that is exactly like the one selected, and pulling from one adds specific files into an already existing repository.
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+    #When I tried to use the python3 command it didn't work, and so I asked one of the peer mentors to help me and we downloaded the missing softwares.
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+    #I'm curious to see exactly how git will be used to share files and how often and when to clone files for assignments in this class.
+
+# 18) Tell me a fun fact!
+    #I like to play Dnd :-)
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(19%3)
+#This prints out the remainder of the first number divided by the second
